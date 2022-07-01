3ec24a7877df4bb52376416f01cd5e11075cee00
v0lkss <108554847+v0lkss@users.noreply.github.com>
2022-07-01 19:09:31 -0300

    Add files via upload

---
+++ wine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+root_verify=$(whoami)
+usuario=$(users)
+
+function init_program() {
+    echo -e "[*] Bienvenido al instalador de CS 1.6..."
+    sleep 1.5
+    sudo apt-get install wine
+    xdg-open "https://www.mediafire.com/file/a9cnfjjhfjy6wbr/Cs_1.6_Full_Espa%25C3%25B1ol_LMT.rar/file"
+}
+
+if [ $root_verify != "root" ]
+then
+    echo -e "[-] Es necesario ejecutar el instalador como super-usuario (root)"
+    echo -e "sudo ./wine.sh"
+else 
+    init_program
+fi
