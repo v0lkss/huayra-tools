5cbfe59d54ea99c4308a8127cffca56814bea412
v0lkss <108554847+v0lkss@users.noreply.github.com>
2022-07-01 19:25:45 -0300

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -6,6 +6,8 @@ function init_program() {
     echo -e "[*] Bienvenido al instalador de CS 1.6..."
     sleep 1.5
     sudo apt-get install wine
+    sleep 1
+    winecfg
     xdg-open "https://www.mediafire.com/file/a9cnfjjhfjy6wbr/Cs_1.6_Full_Espa%25C3%25B1ol_LMT.rar/file"
 }
 
